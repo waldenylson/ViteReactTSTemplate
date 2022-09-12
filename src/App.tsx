713c94f72ac4040950d34ce2b15da76289215b7f
Waldenylson Silva <waldenylson@gmail.com>
2022-09-12 02:47:46 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { createContext } from 'react';
+
+import { RecoilRoot } from 'recoil';
+
+const App: React.FC = () => (
+  <RecoilRoot>
+    <h1>Template Vite React TS (Recoil state managed) </h1>
+  </RecoilRoot>
+);
+
+export default App;
